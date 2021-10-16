cd9a4c1adeb61551269d0fe4f916f6d118e1ab71
vrushabhendra661 <vrushabhendra661@gmail.com>
2021-10-16 22:48:46 +0530

    created code upto skills and added homepage

--- vrushabhendra_portfolio/urls.py
+++ vrushabhendra_portfolio/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from vrushabhendra_portfolioapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',homepage,name='homepage'),
 ]
